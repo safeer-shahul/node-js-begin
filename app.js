68a4ffc6a7944b155d65dcbcefead6a2861eb58b
safeer <safeer.shahul@htpl.cc>
2024-05-06 21:58:24 +0530

    Add form for message input and write received message to message.txt file on POST request to /message. and stored entered message in text file created using fs.writefilesync.

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers)
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');    
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(`<body><form action="/message" method="POST">
+    <input type="text" name="message">
+    <button type="submit">Send</button>
+    </form></body>`);
+    res.write('</html>')
+    return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        const body=[]
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        });
+        req.on('end', ()=>{
+            const parseBody = Buffer.concat(body).toString();
+            console.log(parseBody)
+            const message = parseBody.split('=')[1]
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end() 
+    }
+
+    // console.log(req.url,req.method,req.headers)
     res.setHeader('Content-Type','text/html');
     res.write('<html>');    
     res.write('<head><title>My First Page</title></head>');
